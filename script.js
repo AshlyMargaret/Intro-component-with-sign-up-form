a7bb2347ff7d08b8eb6de416bff4eba48f19e0e9
Ashly Margaret <ashlymargaret030@gmail.com>
2022-09-15 20:15:19 +0530

    Bug Fix: - validation issues resolved - ------------------- - check all fields validation in submit button - show alert on validation sucess

--- script.js
+++ script.js
@@ -8,158 +8,142 @@ let secondName = document.getElementById("lname_id");
 let email = document.getElementById("email_id");
 let password = document.getElementById("pswd_id");
 
-// console.log(input);
-let i;
 
-
-
-form.addEventListener("submit",formSubmission)
-
-function formSubmission(event){
-    // console.log(event);
-    event.preventDefault();
-
-    for(i=0;i<input.length;i++){
-
-        if(input[i].value.length == 0){
-            console.log("hacked");
-            input[i].style.border = "2px solid red";
-            comonError.style.display = "block";   
-        }
-        else if (input[i].value.length != 0){
-            input[i].style.border = "2px solid green"; 
-        }  
-       
-    }
-
-    if(input==input.length){
-        window.location.href ="./sucess.html";
-    }
-     
-
+function bindListners() {
+    firstName.addEventListener("keyup", validateFirstName);
+    firstName.addEventListener("focusout", validateFirstName);
+    secondName.addEventListener("keyup", validateLastName);
+    secondName.addEventListener("focusout", validateLastName);
+    email.addEventListener("keyup", validateEmail);
+    email.addEventListener("focusout", validateEmail);
+    password.addEventListener("keyup", validatePassword);
+    password.addEventListener("focusout", validatePassword);
 }
 
-//first name validation
 
-let firstNameValue;  
-// let fregName = /^[a-zA-Z]+$/;
-let fregName = /^[a-zA-Z ]*$/;
+// function validateField(value) {
+//     if (value == null || value == undefined) {
+//         return false;
+//     }
+//     return true;
 
+// }
 
-firstName.addEventListener("keyup",firstNameValidation)
-
-function firstNameValidation(){
-    firstNameValue = firstName.value;
-    console.log(firstNameValue);
-    if(!firstNameValue.match(fregName)){
-        document.getElementById("error_img1").style.display = "block";
-        document.getElementById("error_msg1").innerHTML = "enter valid name"
+function isNameValid(name) {
+    let nameValidationRegex = /^[a-zA-Z ]*$/;
+    if (name && name.match(nameValidationRegex) && name.trim().length > 0) {
+        return true;
     }
-
-    else{
-        document.getElementById("error_img1").style.display = "none";
-        document.getElementById("error_msg1").style.display = "none";
-    }   
+    return false;
 }
 
- firstName.addEventListener("focusout",firstNameValidation);
 
-
-
- //second name validation
-
-let secondNameValue;  
-let lregName = /^[a-zA-Z ]*$/;
-
-secondName.addEventListener("keyup",secondNameValidation)
-
-function secondNameValidation(){
-    secondNameValue = secondName.value;
-    console.log(secondNameValue);
-    if(!secondNameValue.match(lregName)){
-        document.getElementById("error_img2").style.display = "block";
-        document.getElementById("error_msg2").innerHTML = "enter valid name"
+function validateFirstName() {
+    let firstNameValue = firstName.value;
+    if (isNameValid(firstNameValue)) {
+        // document.getElementById("error_img1").style.display = "none";
+        // document.getElementById("error_msg1").style.display = "none";
+        showErrorForInputField("error_img1", "error_msg1", null)
+        return firstNameValue;
     }
-
-    else{
-        document.getElementById("error_img2").style.display = "none";
-        document.getElementById("error_msg2").style.display = "none";
-    }   
+    showErrorForInputField("error_img1", "error_msg1", "enter valid name");
+    return null;
 }
 
-secondName.addEventListener("focusout",secondNameValidation);
-
-
-//email validation
-
-let emailValue;  
-let Emailreg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-email.addEventListener("keyup",emailValidation)
-
-function emailValidation(){
-    emailValue = email.value;
-    console.log(emailValue);
-    if(!emailValue.match(Emailreg)){
-        document.getElementById("error_img3").style.display = "block";
-        document.getElementById("error_msg3").innerHTML = "enter valid email id";
+function validateLastName() {
+    let lastNameValue = secondName.value;
+    if (isNameValid(lastNameValue)) {
+        // document.getElementById("error_img1").style.display = "none";
+        // document.getElementById("error_msg1").style.display = "none";
+        showErrorForInputField("error_img2", "error_msg2", null)
+        return lastNameValue;
     }
-
-    else{
-        document.getElementById("error_img3").style.display = "none";
-        document.getElementById("error_msg3").style.display = "none";
-    }   
+    showErrorForInputField("error_img2", "error_msg2", "enter valid name");
+    return undefined;
 }
 
-email.addEventListener("focusout",emailValidation);
+
+// Email Validation
+
+function isEmailValid(email) {
+    let emailValidationRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (email && email.match(emailValidationRegex) && email.trim().length > 0) {
+        return true;
+    }
+    return false;
+}
+
+
+function validateEmail() {
+    let emailValue = email.value;
+    if (isEmailValid(emailValue)) {
+        // document.getElementById("error_img1").style.display = "none";
+        // document.getElementById("error_msg1").style.display = "none";
+        showErrorForInputField("error_img3", "error_msg3", null)
+        console.log("emailsucess");
+        return emailValue;
+    }
+    showErrorForInputField("error_img3", "error_msg3", "enter valid email");
+    console.log("emailerror");
+    return undefined;
+}
+
 
 // password validation
 
-password.addEventListener("keyup",passwordValidation)
-
-passwordValue = password.value;
-
-function passwordValidation(){
-    var error = []
-
-    if (passwordValue.length < 8) {
-        error.push("Your password must be at least 8 characters")
-  }
-  else{
-    document.getElementById("error_msg4").style.display = "none";
-  }
-
-
-  if (passwordValue.search(/[a-z]/) < 0) {
-       error.push("Your password must contain at least one lowercase letter.")
-  }
-  else{
-    document.getElementById("error_msg4").style.display = "none";
-  }
-
-
-  if (passwordValue.search(/[A-Z]/) < 0) {
-      error.push("Your password must contain at least one uppercase letter.")
-  }
-  else{
-    document.getElementById("error_msg4").style.display = "none";
-  }
-
-
-//   if (passwordValue.search(/[0-9]/) < 0) {
-//       error.push("Your password must contain at least one digit.")
-//   }
-//   if(passwordValue.search(/[\!\@\#\$\%\^\&\*\(\)\_\+\.\,\;\:\-]/) < 0) {
-//     error.push("Your password must contain at least one special character.")
-//   }
-    
-  if (error.length > 0) {
-    document.getElementById("error_msg4").innerHTML = error.join("<br>")
+function ispasswordValid(pswd) {
+    let passwordValidationRegex = /^(?=.*\d).{4,8}$/;
+    if (pswd && pswd.match(passwordValidationRegex) && pswd.trim().length > 0) {
+        console.log("error pswd1")
+        return true;
+    }
+    console.log("error pswd2")
     return false;
-  }
-  else{
-    document.getElementById("error_msg4").style.display = "none";
-    return true;
-  }
- 
 }
+
+
+function validatePassword() {
+    let passwordValue = password.value;
+    if (ispasswordValid(passwordValue)) {
+        // document.getElementById("error_img1").style.display = "none";
+        // document.getElementById("error_msg1").style.display = "none";
+        showErrorForInputField("error_img4", "error_msg4", null)
+
+        return passwordValue;
+    }
+    showErrorForInputField("error_img4", "error_msg4", "Password must be between 4 and 8 digits long and include at least one numeric digit.");
+
+    return undefined;
+}
+
+
+
+
+function showErrorForInputField(errorImageId, errorMesssageId, error) {
+    // if(error){            // This is equivalent to validateField(error)
+    //     document.getElementById(errorImageId).style.display ="block" 
+    // }else{
+    //     document.getElementById(errorImageId).style.display = "none";
+    // }
+
+
+    document.getElementById(errorImageId).style.display = error ? "block" : "none";
+    document.getElementById(errorMesssageId).innerHTML = error ? error : null;
+}
+
+bindListners();
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+        const firstName = validateFirstName();
+        const secondName = validateLastName();
+        const email = validateEmail();
+        const password = validatePassword();
+        if (firstName && secondName && email && password) {
+            alert(`${firstName} ${secondName} sucessfully registered`);
+        } else {
+            return;
+        }
+
+    
+});
